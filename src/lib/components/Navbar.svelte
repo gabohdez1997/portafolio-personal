02d3352a5aada252fbd54fb77f7d5fe6f199223f
gabohdez1997 <gabohdez1997@gmail.com>
2026-01-17 00:01:20 -0400

    fix: centrado del menu para moviles

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -31,7 +31,7 @@
 </script>
 
 <nav
-	class="fixed top-0 left-0 z-50 flex w-full items-center justify-between border-b border-white/5 bg-black/30 px-6 py-4 backdrop-blur-md"
+	class="fixed top-0 left-0 z-50 flex w-full items-center justify-between border-b border-white/5 bg-black/90 px-6 py-4 backdrop-blur-md md:bg-black/30"
 >
 	<div class="font-display text-xl font-bold tracking-wide">
 		Gabriel<span class="text-[var(--color-primary)]">.dev</span>
@@ -53,7 +53,7 @@
 	<!-- Mobile Menu Button -->
 	<button
 		onclick={toggleMenu}
-		class="relative z-[60] p-2 text-white md:hidden"
+		class="relative z-[100] p-2 text-white md:hidden"
 		aria-label="Toggle Menu"
 	>
 		{#if isOpen}
@@ -67,7 +67,7 @@
 	{#if isOpen}
 		<div
 			transition:fade={{ duration: 200 }}
-			class="fixed inset-0 z-50 flex h-screen w-screen flex-col items-center justify-center bg-black/95 backdrop-blur-xl md:hidden"
+			class="fixed inset-0 z-[90] flex h-screen w-full flex-col items-center justify-center bg-black/95 backdrop-blur-xl md:hidden"
 			onclick={closeMenu}
 			aria-hidden="true"
 		>
